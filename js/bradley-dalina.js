e7155dc09dc72b677372f219fce8334c470e0e22
t3rebra <56643812+t3rebra@users.noreply.github.com>
2019-10-28 06:44:01 -0400

    files updated

---
+++ js/bradley-dalina.js
@@ -0,0 +1,17 @@
+(function (w,d)
+{
+    'use_strict';
+
+    function init() {
+
+    }
+    if (d.addEventListener) {
+        d.addEventListener("DOMContentLoaded", init, false);
+    }
+    w.onload = init;
+    /*@cc_on @*/
+    /*@if (@_win32)
+      document.write('<link id="font-awesome" rel="stylesheet" href="assets/css/font-awesome.min.css" as="style" type="text/css" defer media="all">');
+      document.write('<link id="bradley-dalina-css" rel="stylesheet" href="/css/bradley-dalina.css" as="style" type="text/css" defer media="all">');
+    /*@end @*/
+})(window, document);
